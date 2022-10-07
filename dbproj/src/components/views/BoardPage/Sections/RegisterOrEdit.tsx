6899b945b007a4e7f39d3c45223d82a8a7781f5d
bjs3385 <bjs338570@gmail.com>
2022-10-07 09:57:20 +0900

    .

---
+++ dbproj/src/components/views/BoardPage/Sections/RegisterOrEdit.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function RegisterOrEdit(props: any) {
+    return (
+        <div>
+            <form>
+                <br />
+                <label>Title: </label>
+                <input onChange={props.hadleTitleChange} value={props.titleValue} type="text" name="title" />
+                <hr></hr>
+                <div>
+                    <textarea onChange={props.handleContentChange} value={props.ContentValue} name="content" />
+                </div>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+export default RegisterOrEdit;
--- dbproj/src/components/views/RegisterPage/RegisterPage.tsx
+++ dbproj/src/components/views/RegisterPage/RegisterPage.tsx
@@ -1,34 +1,28 @@
 import React from "react";
 import { useState } from "react";
-import "semantic-ui-css/semantic.min.css";
+import RegisterOrEdit from "../BoardPage/Sections/RegisterOrEdit";
 
 function RegisterPage() {
     const [TitleValue, setTitleVatue] = useState("");
     const [ContentValue, setContentValue] = useState("");
 
-    const onTitleChange = (event) => {
+    const onTitleChange = (event: any) => {
         setTitleVatue(event.currentTarget.value);
     };
     console.log(TitleValue);
 
-    const onContentChange = (event) => {
+    const onContentChange = (event: any) => {
         setContentValue(event.currentTarget.value);
     };
     console.log(ContentValue);
 
     return (
-        <div>
-            <form onSubmit>
-                <br />
-                <label>Title: </label>
-                <input onChange={onTitleChange} value={TitleValue} type="text" name="title" />
-                <hr></hr>
-                <div>
-                    <textarea onChange={onContentChange} value={ContentValue} name="content" />
-                </div>
-                <button onClick>Submit</button>
-            </form>
-        </div>
+        <RegisterOrEdit
+            titleValue={TitleValue}
+            ContentValue={ContentValue}
+            handleTitleChange={onTitleChange}
+            handleContentChange={onContentChange}
+        ></RegisterOrEdit>
     );
 }
 
