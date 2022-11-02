ca0d6baee6da3c783bed14754da19cd43de52d22
bjs3385 <bjs338570@gmail.com>
2022-11-02 19:59:00 +0900

    .

--- dbproj/src/components/views/LoginPage/NewRegisterPage.tsx
+++ dbproj/src/components/views/LoginPage/NewRegisterPage.tsx
@@ -1,28 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import { purple } from "@mui/material/colors";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import InputLabel from "@mui/material/InputLabel";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
+import { koKR } from "@mui/x-date-pickers";
+import koLocale from "date-fns/locale/ko";
 
 function NewRegisterPage() {
-    const [email, setEmail] = React.useState("");
-    const [password1, setPassword1] = React.useState("");
-    const [password2, setPassword2] = React.useState("");
-    const [phonenum, setPhonenum] = React.useState("");
-    const [born, setborn] = React.useState("");
+    const [email, setEmail] = useState("");
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
+    const [phonenum, setPhonenum] = useState("");
+    const [Date, setDate] = useState<Dayjs | null>(dayjs("1999-09-17T21:11:54"));
 
     const onClickRegister = () => {
         if (password1 === password2) {
@@ -32,7 +25,7 @@ function NewRegisterPage() {
                         email: email,
                         password: password1,
                         phonenum: phonenum,
-                        born: born,
+                        Date: Date,
                     },
                 })
                 .then((res) => console.log(res))
@@ -50,6 +43,9 @@ function NewRegisterPage() {
     };
     const handleInputPhone = (e: any) => {
         setPhonenum(e.target.value);
+    };
+    const handleChange = (newValue: Dayjs | null) => {
+        setDate(newValue);
     };
 
     const callApi = async () => {
@@ -92,17 +88,22 @@ function NewRegisterPage() {
                 onChange={handleInputPW2}
             ></TextField>
 
-            <InputLabel id="label1">년</InputLabel>
-            <Select labelId="label1" label="test"></Select>
-
-            <InputLabel id="label2">월</InputLabel>
-            <Select labelId="label2" label="test"></Select>
-
-            <InputLabel id="label3">일</InputLabel>
-            <Select labelId="label3" label="test"></Select>
+            <LocalizationProvider
+                dateAdapter={AdapterDayjs}
+                adapterLocale={koLocale}
+                localeText={koKR.components.MuiLocalizationProvider.defaultProps.localeText}
+            >
+                <DesktopDatePicker
+                    label="생년월일"
+                    inputFormat="MM/DD/YYYY"
+                    value={Date}
+                    onChange={handleChange}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+            </LocalizationProvider>
 
             <TextField
-                label="전화번호"
+                label="번호"
                 required
                 fullWidth
                 name="phonenum"
