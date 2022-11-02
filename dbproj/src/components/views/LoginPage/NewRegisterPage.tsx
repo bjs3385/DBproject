9b6f5890bf4d0cd6f76a8883ca8923695dfbe8d5
bjs3385 <bjs338570@gmail.com>
2022-11-02 20:00:24 +0900

    .

--- dbproj/src/components/views/LoginPage/NewRegisterPage.tsx
+++ dbproj/src/components/views/LoginPage/NewRegisterPage.tsx
@@ -5,13 +5,19 @@ import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
 
 function NewRegisterPage() {
     const [email, setEmail] = React.useState("");
     const [password1, setPassword1] = React.useState("");
     const [password2, setPassword2] = React.useState("");
     const [phonenum, setPhonenum] = React.useState("");
-    const [born, setborn] = React.useState("");
+    const [Date, setDate] = React.useState<Dayjs | null>(
+        dayjs('2014-08-18T21:11:54'),
+      );
 
     const onClickRegister = () => {
         if (password1 === password2 && password1 !== "" && password2 !== "") {
@@ -21,13 +27,13 @@ function NewRegisterPage() {
                         email: email,
                         password: password1,
                         phonenum: phonenum,
-                        born: born,
+                        date: Date,
                     },
                 })
                 .then((res) => console.log(res))
                 .catch();
         } else if (password1 !== password2) {
-            alert("비밀번호가 일치하지 않습니다ㄴㅁㅇ");
+            alert("비밀번호가 일치하지 않습니다.");
         } else if(email === ""){
             alert("이메일을 입력해주세요");
         } else if(password1 === ""){
@@ -36,7 +42,7 @@ function NewRegisterPage() {
             alert("비밀번호를 입력해주세요");
         } else if(phonenum === ""){
             alert("전화번호를 입력해주세요");
-        } else if(born === ""){
+        } else if(Date === null){
             alert("생년월일을 입력해주세요");
         }
         
@@ -53,6 +59,11 @@ function NewRegisterPage() {
     const handleInputPhone = (e: any) => {
         setPhonenum(e.target.value);
     };
+    
+    
+      const handleChange = (newValue: Dayjs | null) => {
+        setDate(newValue);
+      };
 
     const callApi = async () => {
         axios.get("/users").then((res) => console.log(res.data));
@@ -94,14 +105,16 @@ function NewRegisterPage() {
                 onChange={handleInputPW2}
             ></TextField>
 
-            <InputLabel id="label1">년</InputLabel>
-            <Select labelId="label1" label="test"></Select>
-
-            <InputLabel id="label2">월</InputLabel>
-            <Select labelId="label2" label="test"></Select>
-
-            <InputLabel id="label3">일</InputLabel>
-            <Select labelId="label3" label="test"></Select>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        
+                <DesktopDatePicker
+          label="Date desktop"
+          inputFormat="MM/DD/YYYY"
+          value={Date}
+          onChange={handleChange}
+          renderInput={(params: any) => <TextField {...params} />}
+        />
+        </LocalizationProvider>
 
             <TextField
                 label="전화번호"
