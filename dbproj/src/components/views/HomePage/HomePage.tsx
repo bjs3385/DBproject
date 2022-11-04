267b9a97849f944f410cdd73742ffc9a66e2acaf
bjs3385 <bjs338570@gmail.com>
2022-11-04 21:13:40 +0900

    .

--- dbproj/src/components/views/HomePage/HomePage.tsx
+++ dbproj/src/components/views/HomePage/HomePage.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 function HomePage() {
     return (
         <Container fixed>
+            <img src="https://thumbnail10.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/6195/993b4df0be8b5f18aa0965b9ea859afc92eb25b77d27f461613dc9176387.jpg"></img>
             <Logout></Logout>
             <Box sx={{ height: "100vh" }}>HomePage</Box>
             <Box sx={{ bgcolor: "#cfe8fc", height: "200vh" }} />
