227ac5c05953670a2083091ff35ddccc52c42426
bjs3385 <bjs338570@gmail.com>
2022-11-15 23:02:13 +0900

    1

--- dbproj/server/routes/setitem.js
+++ dbproj/server/routes/setitem.js
@@ -7,9 +7,18 @@ router.get("/", function (req, res, next) {
     res.send({ test: "hi" });
 });
 
-router.post("/getItem", function(req, res, next) {
-    const boardId = req.query.boardId;
-    console.log("boardId: " + boardId);
+router.get("/getItem", function (req, res, next) {
+    console.log("getItem");
+    connection.query("SELECT * FROM product", (err, rows, fields) => {
+        if (err) throw err;
+        if (rows.length > 0) {
+            console.log("success");
+            res.send({ result: true, rows: rows });
+        } else {
+            console.log("error");
+            res.send({ result: false });
+        }
+    });
 });
 
 module.exports = router;
--- dbproj/src/components/views/ArticlePage/ImagePage.tsx
+++ dbproj/src/components/views/ArticlePage/ImagePage.tsx
@@ -1,65 +1,55 @@
 import React, { useEffect, useState } from "react";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import axios from "axios";
 
-
-
-function ImagePage(props : any) {
-
-    const [data, setdata] = useState([{
-        id: 0,
-        imageLink: "",
-        imageName: "test",
-        itemName:  "test",
-        itemPrice: 0,
-        itemDescription: "test",
-    }]);
-    
-    
+function ImagePage(props: any) {
+    const [data, setdata] = useState([]);
+    const test = {
+        width: "500px",
+        height: "500px",
+    };
     const callApi = async () => {
         const res = await axios.get("/setitem/getItem");
-        const result = res.data.result;
+        const result = res.data.rows;
+
         setdata(result);
     };
-
+    console.log(data);
     useEffect(() => {
-
         callApi();
-    },[]);
+    }, []);
 
-    
     return (
-        <div>
-        {data.map((row: any) => (
-            <div key = "row.id + 5">
-    <Card sx={{ maxWidth: 345 }}>
-        <CardMedia
-            key = "{ row.id }"
-          component="img"
-          height="140"
-          image="{ row.imageLink }"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" key = "{row.id + 3 }">
-            { row.itemName }
-          </Typography>
-          <Typography variant="body2" color="text.secondary" key = "{row.id + 4 }">
-            { row.itemDescription }
-          </Typography>
-        </CardContent>
-        <CardActions >
-          <Button key = "{row.id + 1 }" size="small">Share</Button>
-          <Button key = "{row.id + 2 }"size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-      </div>
-      ))}
-      </div>
+        <div style={test}>
+            {data.map((row: any) => (
+                <div key={row.pID + 5}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia key={row.pID + 0} component="img" height="140" image={row.pIMAGE1} />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div" key={row.pID + 3}>
+                                {row.pNAME}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary" key={row.pID + 4}>
+                                {row.pDETAIL}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button key={row.pID + 1} size="small">
+                                Share
+                            </Button>
+                            <Button key={row.pID + 2} size="small">
+                                Learn More
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </div>
+            ))}
+        </div>
     );
 }
 
