ff6e4503c857d233420944058f60f79deb7ee7ac
Kwon <seedksk@gmail.com>
2022-11-23 23:48:52 +0900

    홈페이지버튼구현

--- dbproj/src/components/views/HomePage/HomePage.tsx
+++ dbproj/src/components/views/HomePage/HomePage.tsx
@@ -2,16 +2,41 @@ import React, { useEffect } from "react";
 import Logout from "../LoginPage/Logout";
 import MyPage from "../ArticlePage/MyPagebtn";
 import { Box, Container } from "@mui/system";
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import axios from "axios";
 function HomePage() {
+    const onclickMyPage = () => {
+        window.location.replace("/mypage");
+    }
+    const onclickLogout = () => {
+        localStorage.clear();
+        window.location.replace("/login");
+    }
+    const loginState = false;
     return (
         <Container fixed>
-            <img src="https://thumbnail10.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/6195/993b4df0be8b5f18aa0965b9ea859afc92eb25b77d27f461613dc9176387.jpg"></img>
-            <Logout></Logout>
-            <MyPage></MyPage>
+            <PopupState variant="popover" popupId="demo-popup-menu">
+                {(popupState) => (
+                    <React.Fragment>
+                    <Button variant="contained" >
+                    <AccountCircleIcon {...bindTrigger(popupState)}></AccountCircleIcon>
+                    </Button>
+                    <Menu {...bindMenu(popupState)}>
+                        <MenuItem onClick={() => onclickMyPage()}>My Page</MenuItem>
+                        <MenuItem divider={loginState} onClick={() => onclickLogout()}>Log out</MenuItem>
+                        <MenuItem onClick={() => onclickMyPage()}>asd</MenuItem>
+                    </Menu>
+                    </React.Fragment>
+                )}
+            </PopupState>
             <Box sx={{ height: "100vh" }}>HomePage</Box>
             <Box sx={{ bgcolor: "#cfe8fc", height: "200vh" }} />
         </Container>
+        
     );
 }
 export default HomePage;
