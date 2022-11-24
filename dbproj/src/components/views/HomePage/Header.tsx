c2aaa8ee82770e6933465a225dc13c2c8416bfb0
bjs3385 <bjs338570@gmail.com>
2022-11-24 16:21:30 +0900

    .

--- dbproj/src/components/views/HomePage/Header.tsx
+++ dbproj/src/components/views/HomePage/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <div id="Header">
             <p>----Header----</p>
-            <h1 id="Header-logo"><a href="/index.html"><img src="https://www.brooksbrothers.co.kr/images/common/logo_m.png" alt="브룩스브라더스"/></a></h1>
+            <h1 id="Header-logo"><a href="/"><img src="https://www.brooksbrothers.co.kr/images/common/logo_m.png" alt="브룩스브라더스"/></a></h1>
             <div id="Header-menu">
                 <span><a href="#">로그인</a></span>
                 <span><a href="#">회원가입</a></span>
